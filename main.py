45c80398287478cd68ba6caa43dc77b9938c3ca4
raul <raulzambranoleon15@gmail.com>
2025-10-30 02:11:08 -0500

    Reconocimiento de coordenadas, no analiza numenos en la lista de palabras comunes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Importamos las librerías necesarias
 from watchdog.observers import Observer       # Permite observar cambios en archivos y carpetas
 from watchdog.events import FileSystemEventHandler  # Clase base para manejar eventos de archivos
-import time, os, re, spacy
+import time, os, re
 
 patron_celular=re.compile(r'\b9\d{8}\b')
 
 patron_dni=re.compile(r'\b\d{8}\b')
+
+patron_coordenadas=re.compile(r'[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)[ ,]+[-+]?(180(\.0+)?|((1[0-7]\d)|(\d{1,2}))(\.\d+)?)')
 
 
 #extraemos palabras comunes del diccionario
@@ -18,19 +20,21 @@ palabras_comunes = set(lista)
 
 
 def reconoce_nombre(fila: str) -> str:
+ 
     palabras = fila.lower().split()
     # Casos base
-    if not palabras:
+    if not palabras or fila.strip().isdigit():
+            
         return fila
 
-    encabezados = {'nombre:', 'dni:', 'celular:', '####################'}
+    encabezados = {'nombre:', 'dni:', 'celular:', '####################', 'coordenadas:'}
     if palabras[0] in encabezados:
         return fila
 
     # Si todas las palabras no están en el conjunto común, se asume nombre
     es_nombre = all(p not in palabras_comunes for p in palabras)
     if es_nombre:
-        return f"NOMBRE: {' '.join(palabras).upper()}\n".rstrip()
+        return f'NOMBRE: {' '.join(palabras).upper()}'.rstrip()+'\n'
     
     return ' '.join(palabras).upper().rstrip()+'\n'
 
@@ -44,6 +48,11 @@ def reconoce_dni(texto):
 def reconoce_celular(texto):
     m = patron_celular.search(texto)
     return f'CELULAR: {m.group(0)}'.rstrip()+'\n' if m else texto
+
+def reconoce_coordenadas(coordenadas):
+    m=patron_coordenadas.search(coordenadas)
+    
+    return f'COORDENADAS: {m.group(0)}'.strip()+'\n' if m else coordenadas
 
 # Definimos una clase que manejará los eventos de modificación de archivos
 class MiHandler(FileSystemEventHandler):
@@ -88,6 +97,10 @@ class MiHandler(FileSystemEventHandler):
                     
                     #procesamos numeros de dni                   
                     nuevas=[reconoce_dni(dni) for dni in nuevas]
+                    
+                    #procesamos coordenadas
+                    nuevas=[reconoce_coordenadas(coordenadas) for coordenadas in nuevas]
+                    
                     #procesamos nombres
                     nuevas=[reconoce_nombre(nombre) for nombre in nuevas]
                    
